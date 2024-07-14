400abfa339952c404f4303733b944ad2676119e4
Gina James <gina.james@myob.com>
2024-07-14 13:49:19 +1000

    WIP 2

--- Concept Diagrams/ci_cd_diagram/main.py
+++ Concept Diagrams/ci_cd_diagram/main.py
@@ -3,5 +3,5 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Web Service", show=False):
+with Diagram("Web Service Example", show=False):
     ELB("lb") >> EC2("web") >> RDS("userdb")
