abfbdfcc4518e462a64564d96819a8f320466871
Gina James <gina.james@myob.com>
2024-07-15 22:07:55 +1000

    Add graphiz install step

--- Concept_Diagrams/src/main.py
+++ Concept_Diagrams/src/main.py
@@ -3,5 +3,16 @@ from diagrams.aws.compute import EC2
 from diagrams.aws.database import RDS
 from diagrams.aws.network import ELB
 
-with Diagram("Web Service Example", show=False):
+#filenamePrefix = "diagram_"
+#filenameNumber[]
+with Diagram("WebExample", show=False, filename="my_diagram"):
     ELB("lb") >> EC2("web") >> RDS("userdb")
+#    filenameNumber.append((len)filenameNumber + 1)
+
+# maybe have a list to append the names of the diagrams to?
+# then use that name for the diagram image file name
+# make a diagram of this project lol
+# make diagrams save in generated_diagrams folder
+
+with Diagram("Simple Diagram", show=False):
+    EC2("web") >> RDS("your mom")
